9f37d38a07f31fa1bb67150f32dda7d51b9d3b4f
Alan Dell'Onze <a.dellonze@techgap.it>
2017-11-25 00:01:42 +0100

    using ade-vue-dynamic-form

--- src/components/Page2.vue
+++ src/components/Page2.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <df-form :templates="templates" :onSubmit="onSubmit" />
   </div>
 </template>
 
@@ -9,7 +10,33 @@ export default {
   name: 'Page2',
   data () {
     return {
-      msg: 'Page2'
+      msg: 'Page2',
+      templates: [
+        {
+          type: 'text',
+          value: 'value text',
+          maxLength: '10',
+          required: true
+        }, {
+          type: 'text',
+          value: 'value text 2'
+        }, {
+          type: 'textarea',
+          value: 'value textarea',
+          maxLength: '20'
+        }, {
+          type: 'image',
+          value: 'http://www.tutorialsavvy.com/wp-content/uploads/2015/10/image_thumb23.png'
+        }
+      ]
+    }
+  },
+
+  methods: {
+    onSubmit () {
+      this.templates.map(function (template) {
+        console.log(template.type + ', ' + template.value)
+      })
     }
   }
 }
